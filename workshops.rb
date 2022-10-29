e78e1e8028e847700bc09370cb4258d0a98bb23a
JoanAlvarado040700 <sevaslarios@gmail.com>
2022-10-29 00:07:03 -0600

    Inicio basico

---
+++ workshops.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+
+def workshops_content(name)
+    File.read("workshops/#{name}.txt")
+
+rescue Erno::ENOENT
+    return nil
+end
+
+
+get '/' do
+
+    @files = Dir.entries("workshops")
+    erb :index
+
+end
+
+
+get '/:name' do
+    @name =params[:name]
+    @descriptions = workshops_content(@name)
+    erb :workshops
+end
